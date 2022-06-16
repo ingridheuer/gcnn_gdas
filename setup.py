3bedcfef311112a024ad8402ff64d31a94a8c212
Ingrid Heuer <ingridheuer94@gmail.com>
2022-06-16 17:11:06 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Graph Convolutional Neural Networks for predictions of new gene-disease associations',
+    author='ingrid heuer',
+    license='',
+)
