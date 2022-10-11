444331b07a48df3d5a1f942d6e51e0e2f719f0c3
Ingrid Heuer <ingridheuer94@gmail.com>
2022-10-11 15:15:55 -0300

    arregle manual maps

--- src/data/add_manual_maps.py
+++ src/data/add_manual_maps.py
@@ -11,7 +11,7 @@ prime_grouped_bert = pd.read_csv(data_external + "kg_grouped_diseases_bert_map.c
 
 manual_maps = pd.merge(manual_maps[["diseaseId","code","name","node_index","vocabularyName"]], prime_grouped_bert[["node_id","group_id_bert","group_name_bert","group_name_auto"]],left_on="code",right_on="node_id",how="left").drop(columns="node_id")
 disease_mappings = disease_mappings.set_index("diseaseId").drop(manual_maps.diseaseId.values).reset_index()
-disease_mappings = pd.concat([disease_mappings,manual_maps])
+disease_mappings = pd.concat([disease_mappings,manual_maps]).rename(columns={"node_index":"prime_node_index","code":"mondo_id"})
 
 disease_mappings.to_csv(data_processed+"graph_data/disease_mappings.csv")
 # %%
