25c409f66d3003f78dcf51863216ab85a4fffa96
Ingrid Heuer <ingridheuer94@gmail.com>
2022-11-07 14:26:06 -0300

    refactored nlp analysis + added config toml

--- exploration/visualizations/cluster_finder.py
+++ exploration/visualizations/cluster_finder.py
@@ -2,10 +2,11 @@
 import pandas as pd
 import numpy as np
 #%%
+graph_data = "../../data/processed/graph_data_nohubs/"
 reports_tfidf = "../../reports/reports_nohubs/analisis_tfidf/"
 reports_lsa = "../../reports/reports_nohubs/analisis_lsa/"
-louvain_analysis = pd.read_pickle(reports_tfidf+"louvain_analysis_checkpoint.pkl")
-infomap_analysis = pd.read_pickle(reports_tfidf+"infomap_analysis_checkpoint.pkl")
+louvain_analysis = pd.read_pickle(graph_data+"louvain_top_terms.pkl")
+infomap_analysis = pd.read_pickle(graph_data+"infomap_top_terms.pkl")
 #%%
 
 def match_keyword_to_score(word_col,score_col,keyword):
@@ -21,16 +22,16 @@ def find_cluster(partition, keyword:str):
     else: 
         print("Not a valid partition")
     
-    monogram_match =  df[df.top_5_monograms.apply(lambda x: keyword in x)]
-    monogram_match["score"] = monogram_match.apply(lambda x: match_keyword_to_score(x.top_5_monograms, x.top_5_monograms_score, keyword), axis=1)
+    monogram_match =  df[df.top_5_monogram.apply(lambda x: keyword in x)]
+    monogram_match["score"] = monogram_match.apply(lambda x: match_keyword_to_score(x.top_5_monogram, x.top_5_monogram_score, keyword), axis=1)
     monogram_match = monogram_match.sort_values(by="score", ascending=False)
 
-    bigram_match = df[df.top_5_bigrams.apply(lambda x: any(keyword in s for s in x))]
-    bigram_match["score"] = bigram_match.apply(lambda x: match_keyword_to_score(x.top_5_bigrams, x.top_5_bigrams_score, keyword), axis=1)
+    bigram_match = df[df.top_5_bigram.apply(lambda x: any(keyword in s for s in x))]
+    bigram_match["score"] = bigram_match.apply(lambda x: match_keyword_to_score(x.top_5_bigram, x.top_5_bigram_score, keyword), axis=1)
     bigram_match = bigram_match.sort_values(by="score", ascending=False)
 
-    trigram_match = df[df.top_5_trigrams.apply(lambda x: any(keyword in s for s in x))]
-    trigram_match["score"] = trigram_match.apply(lambda x: match_keyword_to_score(x.top_5_trigrams, x.top_5_trigrams_score, keyword), axis=1)
+    trigram_match = df[df.top_5_trigram.apply(lambda x: any(keyword in s for s in x))]
+    trigram_match["score"] = trigram_match.apply(lambda x: match_keyword_to_score(x.top_5_trigram, x.top_5_trigram_score, keyword), axis=1)
     trigram_match = trigram_match.sort_values(by="score", ascending=False)
 
     result = pd.concat([monogram_match,bigram_match,trigram_match]).drop_duplicates(subset=["comunidad"], keep="first")
@@ -38,27 +39,4 @@ def find_cluster(partition, keyword:str):
 
     return result
 #%%
-find_cluster("louvain","retinitis")
-#%%
-data_processed = "../../data/processed/"
-data_external = "../../data/external/"
-graph_data = data_processed + "graph_data_nohubs/"
-
-graph_node_data = pd.read_csv(graph_data+"nohub_graph_node_data.csv")
-graph_edge_data = pd.read_csv(graph_data+"nohub_graph_edge_data.csv").rename(columns={"relation":"edge_type"})
-
-retinitis = graph_node_data[graph_node_data.comunidades_louvain == 54]
-joubert = graph_node_data[graph_node_data.comunidades_louvain == 20]
-#%%
-disease_attributes = pd.read_csv(graph_data+"nohub_disease_attributes.csv")
-orig_disgenet = pd.read_csv(data_external+"disease_mappings_to_attributes.tsv",sep="\t")
-curated_edges = pd.read_csv(data_external+"curated_gene_disease_associations.tsv", sep="\t")
-#%%
-retinitis_ejemplos = [29377, 20693, 27388, 30769]
-retinitis_ejemplos_ids = disease_attributes.set_index("node_index").loc[retinitis_ejemplos, "node_id"].values
-
-graph_edge_data.set_index("x_index").loc[retinitis_ejemplos]
-orig_disgenet.set_index("diseaseId").loc[retinitis_ejemplos_ids]
-curated_edges.set_index("diseaseId").loc[retinitis_ejemplos_ids]
-#%%
-node_labels = pd.read_csv(reports_lsa+"kmeans_clustering_labels.csv")
+find_cluster("infomap","retinitis")
