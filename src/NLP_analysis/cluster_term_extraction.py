7c63fb18661871a14601d716608d41e0a3e113b9
Ingrid Heuer <ingridheuer94@gmailcom>
2022-11-08 12:55:45 -0300

    random cluster entropy implementado

--- src/NLP_analysis/cluster_term_extraction.py
+++ src/NLP_analysis/cluster_term_extraction.py
@@ -1,8 +1,8 @@
 #%%
 import numpy as np
 import pandas as pd
-from scipy import sparse
 import random
+import nlp_utils
 #%%
 seed = 16
 random.seed(seed)
@@ -16,44 +16,12 @@ path_infomap = graph_data + "tfidf_infomap/"
 path_louvain = graph_data + "tfidf_louvain/"
 
 graph_node_data = pd.read_csv(graph_data+"nohub_graph_node_data.csv")
-tamaños_louvain = graph_node_data.comunidades_louvain.dropna().value_counts()
-tamaños_infomap = graph_node_data.comunidades_infomap.dropna().value_counts()
-
-infomap_clusters = pd.DataFrame(tamaños_infomap).reset_index().rename(columns={"index":"comunidad","comunidades_infomap":"tamaño"}).astype({"comunidad":"int"})
-louvain_clusters = pd.DataFrame(tamaños_louvain).reset_index().rename(columns={"index":"comunidad","comunidades_louvain":"tamaño"}).astype({"comunidad":"int"})
+infomap_clusters, louvain_clusters = nlp_utils.get_cluster_dataframes(graph_node_data)
 
 infomap_ids = infomap_clusters.comunidad.values
 louvain_ids = louvain_clusters.comunidad.values
 
 #%%
-
-def load_sparse_dataframe(matrix_path,row_path,column_path):
-    mat = sparse.load_npz(matrix_path)
-    row = np.loadtxt(row_path)
-    col = np.loadtxt(column_path, dtype="str")
-    df = pd.DataFrame.sparse.from_spmatrix(mat, index=row, columns=col)
-    return df
-
-def load_cluster_matrices(path_infomap:str,path_louvain:str,number_of_matrices):
-    tfidf_infomap = []
-    tfidf_louvain = []
-
-    for i in range(number_of_matrices):
-        mat_path = f"{path_infomap}matriz_tfidf_infomap_{i}.npz"
-        row_path = f"{path_infomap}rows_tfidf_infomap_{i}.txt"
-        col_path = f"{path_infomap}cols_tfidf_infomap_{i}.txt"
-        
-        tfidf_infomap.append(load_sparse_dataframe(mat_path,row_path,col_path))
-
-    for i in range(number_of_matrices):
-        mat_path = f"{path_louvain}matriz_tfidf_louvain_{i}.npz"
-        row_path = f"{path_louvain}rows_tfidf_louvain_{i}.txt"
-        col_path = f"{path_louvain}cols_tfidf_louvain_{i}.txt"
-        
-        tfidf_louvain.append(load_sparse_dataframe(mat_path,row_path,col_path))
-    
-    return tfidf_infomap, tfidf_louvain
-
 def get_top_terms(matrix,cluster,number_of_terms):
     top_terms = matrix.loc[cluster].sort_values(ascending=False)[0:number_of_terms].index.values.astype(str)
     top_terms_scores = np.around(matrix.loc[cluster].sort_values(ascending=False)[0:number_of_terms].values, decimals=2)
@@ -77,7 +45,7 @@ def top_ngrams(matrix_list,matrix_names_list,clusters,number_of_terms):
 
 #%%
 print("Loading cluster matrices ...")
-tfidf_infomap, tfidf_louvain = load_cluster_matrices(path_infomap,path_louvain,4)
+tfidf_infomap, tfidf_louvain = nlp_utils.load_cluster_matrices(path_infomap,path_louvain,4)
 
 tfidf_infomap_dense = [mat.sparse.to_dense() for mat in tfidf_infomap]
 tfidf_louvain_dense = [mat.sparse.to_dense() for mat in tfidf_louvain]
