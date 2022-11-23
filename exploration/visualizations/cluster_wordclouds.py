f6db3c06cdf4e3afed255a26fb0043b46f8cf23a
Ingrid Heuer <ingridheuer94@gmail.com>
2022-11-23 14:52:16 -0300

    truncated dtm for clusters

--- exploration/visualizations/cluster_wordclouds.py
+++ exploration/visualizations/cluster_wordclouds.py
@@ -1,67 +1,50 @@
 #%%
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import plotly.express as px
-from scipy import sparse
+# import plotly.express as px
 from wordcloud import WordCloud
 #%%
-data_processed = "../../data/processed/"
-data_interim = "../../data/interim/"
-data_external = "../../data/external/"
-graph_data = data_processed + "graph_data_nohubs/"
-reports_tfidf = "../../reports/reports_nohubs/analisis_tfidf/"
-#%%
-def load_sparse_dataframe(matrix_path,row_path,column_path,cols_str=True):
-    mat = sparse.load_npz(matrix_path)
-    row = np.loadtxt(row_path)
-    if cols_str:
-        col = np.loadtxt(column_path, dtype="str")
-    else:
-        col = np.loadtxt(column_path)
-        
-    df = pd.DataFrame.sparse.from_spmatrix(mat, index=row, columns=col)
-    return df
+def load_dtm_matrices(path,name,N):
+    term_path = f"{path}top_{N}_terms_{name}.csv"
+    score_path = f"{path}top_{N}_score_{name}.csv"
+    term_matrix = pd.read_csv(term_path, index_col=0)
+    score_matrix = pd.read_csv(score_path, index_col=0)
+    return term_matrix, score_matrix
 
-#%%
-louvain_path = graph_data + "tfidf_louvain/"
-infomap_path = graph_data + "tfidf_infomap/"
-
-louvain_term_matrix = load_sparse_dataframe(louvain_path+"matriz_tfidf_louvain_0.npz", louvain_path+"rows_tfidf_louvain_0.txt",louvain_path+"cols_tfidf_louvain_0.txt")
-infomap_term_matrix = load_sparse_dataframe(infomap_path+"matriz_tfidf_infomap_0.npz", infomap_path+"rows_tfidf_infomap_0.txt",infomap_path+"cols_tfidf_infomap_0.txt")
-
-dense_dat_louvain = louvain_term_matrix.sparse.to_dense()
-dense_dat_infomap = infomap_term_matrix.sparse.to_dense()
-#%%
-def plot_wordcloud(partition,cluster,num_terms,cmap,background_color="white"):
+def plot_wordcloud(partition,cluster,cmap,background_color="white"):
     if partition == "infomap":
-        cluster_term_matrix = dense_dat_infomap
+        terms,scores = load_dtm_matrices("../../reports/summary/","infomap",100)
     elif partition == "louvain":
-        cluster_term_matrix = dense_dat_louvain
+        terms,scores = load_dtm_matrices("../../reports/summary/","louvain",100)
     else:
         print("Not a valid partition")
 
-    cluster_series = cluster_term_matrix.loc[cluster].sort_values(ascending=False)[0:num_terms]
-    wordcloud = WordCloud(colormap=cmap,background_color=background_color).generate_from_frequencies(frequencies=cluster_series)
+    cluster_terms = terms.loc[cluster]
+    cluster_scores = scores.loc[cluster]
+    frequencies = dict(zip(cluster_terms,cluster_scores))
+    wordcloud = WordCloud(colormap=cmap,background_color=background_color).generate_from_frequencies(frequencies=frequencies)
     plt.figure()
     plt.imshow(wordcloud, interpolation="bilinear")
-    plt.axis("off")
+    plt.axis("off") 
     plt.show()
 
-def plot_term_distribution(partition,cluster):
+# def plot_term_distribution(partition,cluster):
+#     if partition == "infomap":
+#         terms,scores = load_dtm_matrices("../../reports/summary/","infomap",100)
+#     elif partition == "louvain":
+#         terms,scores = load_dtm_matrices("../../reports/summary/","louvain",100)
+#     else:
+#         print("Not a valid partition")
+    
+#     cluster_terms = terms.loc[cluster]
+#     cluster_scores = scores.loc[cluster]
+#     frequencies = dict(zip(cluster_terms,cluster_scores))
 
-    if partition == "infomap":
-        cluster_term_matrix = dense_dat_infomap
-    elif partition == "louvain":
-        cluster_term_matrix = dense_dat_louvain
-    else:
-        print("Not a valid partition")
-
-    fig = px.bar(cluster_term_matrix.loc[cluster].sort_values(ascending=False)[0:10],width=800, height=400, title="TF-IDF monogram distribution").update_layout(yaxis_title="Monograms",xaxis_title="TF-IDF value")
-    fig.show()
+#     fig = px.bar(frequencies,width=800, height=400, title="TF-IDF monogram distribution").update_layout(yaxis_title="Monograms",xaxis_title="TF-IDF value")
+#     fig.show()
 
 #%%
-plot_wordcloud("infomap",1021,70,None,"white")
+plot_wordcloud("infomap",525,None,"white")
 #%%
-plot_wordcloud("louvain",11,70,None,"white")
+plot_wordcloud("louvain",31,None,"white")
 #%%
