4a0015d271739b11ae7d6ab34c1031c71ee0e990
Ingrid Heuer <ingridheuer94@gmail.com>
2023-03-23 17:42:10 -0300

    demo random link split

--- exploration/model_exploration/create_subgraph.py
+++ exploration/model_exploration/create_subgraph.py
@@ -1,3 +1,5 @@
+# Create a subgraph for demos
+
 #%%
 import pandas as pd 
 import networkx as nx
@@ -8,12 +10,17 @@ node_data = pd.read_csv(data_processed+"nohub_graph_nodes.csv")
 
 G = nx.from_pandas_edgelist(edge_data,source="x_index",target="y_index")
 node = 600
+# node = 50
+# node = 100
 g = nx.ego_graph(G,node,2)
 nodelist = list(g.nodes())
-
+print(len(nodelist))
+print(g.number_of_edges())
+#%%
 subgraph_node_data = node_data.set_index("node_index").loc[nodelist]
 subgraph_edge_data_temp = edge_data.set_index("x_index").loc[nodelist].reset_index()
 subgraph_edge_data = subgraph_edge_data_temp.set_index("y_index").loc[nodelist].reset_index()
 
 subgraph_node_data.to_csv(data_processed+"subgraph_nodes.csv")
 subgraph_edge_data.to_csv(data_processed+"subgraph_edges.csv")
+#%%
--- exploration/model_exploration/demo_linkspĺit.py
+++ exploration/model_exploration/demo_linkspĺit.py
@@ -54,13 +54,13 @@ def create_heterodata(node_map, edge_index):
     return data
 
 def tensor_to_edgelist(tensor: torch.tensor):
-    "Toma un edge_index de shape (2,num_edges) y devuelve una lista de tuplas"
     sources = tensor[0,:].tolist()
     targets = tensor[1,:].tolist()
     edgelist = list(zip(sources,targets))
     return edgelist
 
 def reverse_map(node_map,edge_list,edge_type):
+    """Maps edge dictionary from pyg Heterodata back into the original node indexes from the dataframe"""
     src_map = {v:k for k,v in node_map[edge_type[0]].items()}
     dst_map = {v:k for k,v in node_map[edge_type[2]].items()}
     mapped_edge_list = [(src_map[n1],dst_map[n2]) for (n1,n2) in edge_list]
@@ -68,6 +68,7 @@ def reverse_map(node_map,edge_list,edge_type):
     return mapped_edge_list
 
 def inverse_map_heterodata(data,node_map):
+    """Maps full edge data from pyg Heterodata back into the original node indexes from the dataframe"""
     edge_dict = {}
     for edge_type in data.edge_types:
         type_dict = {}
@@ -95,43 +96,9 @@ def inverse_map_heterodata(data,node_map):
         edge_dict[edge_type] = type_dict
     
     return edge_dict
-#%%
-data_folder = "../../data/processed/graph_data_nohubs/"
-node_data, node_map = load_node_csv(data_folder+"subgraph_nodes.csv","node_index","node_type")
-edge_data, edge_index = load_edge_csv(data_folder+"subgraph_edges.csv","x_index","y_index",node_map,"edge_type","x_type","y_type")
-data = create_heterodata(node_map,edge_index)
-#%%
-edge_types = [('disease', 'disease_disease', 'bert_group'),('gene_protein', 'gda', 'disease'),('gene_protein', 'ppi', 'gene_protein')]
-rev_edge_types = [('bert_group', 'disease_disease', 'disease'),('disease', 'gda', 'gene_protein'),('gene_protein', 'ppi', 'gene_protein')]
 
-good_transform = RandomLinkSplit(num_val=0.3, num_test=0.3, is_undirected=True, add_negative_train_samples=True, disjoint_train_ratio=0.2,edge_types=edge_types,rev_edge_types=rev_edge_types)
-
-bad_transform = RandomLinkSplit(num_val=0.3, num_test=0.3, is_undirected=True, add_negative_train_samples=True, disjoint_train_ratio=0.2,edge_types=data.edge_types)
-
-g_train, g_val, g_test = good_transform(data)
-b_train, b_val, b_test = bad_transform(data)
-#%%
-# G = nx.DiGraph()
-G = nx.MultiDiGraph()
-G.add_nodes_from(node_data.index.values)
-nx.set_node_attributes(G,node_data["node_type"].to_dict(),"node_type")
-#%%
-# def add_edges_from_pyg(G,data,node_map):
-#     H = G.copy()
-#     edge_dict = inverse_map_heterodata(data,node_map)
-#     for edge_type,dictionary in edge_dict.items():
-#         for subtype, edgelist in dictionary.items():
-#             if subtype != "supervision_labels":
-#                 if "supervision_labels" in dictionary.keys():
-#                     sup_labels = {e:label for e,label in zip(dictionary["supervision_edges"],dictionary["supervision_labels"])}
-#                     edges = [(n1,n2,{"subtype":subtype, "type":edge_type[1], "supervision_label":sup_labels[(n1,n2)]}) for (n1,n2) in edgelist]
-#                     H.add_edges_from(edges)
-#                 else:
-#                     edges = [(n1,n2,{"subtype":subtype, "type":edge_type[1], "supervision_label":1}) for  (n1,n2) in edgelist]
-#                     H.add_edges_from(edges)
-#     return H
-#%%
 def add_edges_from_pyg(G,data,node_map):
+    """Imports edges from pyg Heterodata splits into a Networkx graph (assumes both graphs have the same nodes)"""
     H = G.copy()
     edge_dict = inverse_map_heterodata(data,node_map)
     for edge_type,dictionary in edge_dict.items():
@@ -143,7 +110,7 @@ def add_edges_from_pyg(G,data,node_map):
             supervision_edges = [(n1,n2,{"subtype":"supervision_edges", "type":edge_type[1], "supervision_label":sup_labels[(n1,n2)]}) for (n1,n2) in dictionary["supervision_edges"]]
             H.add_edges_from(supervision_edges)
     return H
-#%%
+
 def generate_style_list(G,key,rule_dict,nodes):
     if nodes:
         style_list = [rule_dict[case] for case in nx.get_node_attributes(G,key).values()]
@@ -156,28 +123,72 @@ def draw_graph(g,ax,node_color_rule,edge_color_rule,edge_line_rule,layout):
     edge_colors = generate_style_list(g,edge_color_rule[0],edge_color_rule[1],False)
     edge_lines = generate_style_list(g,edge_line_rule[0],edge_line_rule[1],False)
 
-    nx.draw(g,ax=ax,pos=layout,node_color=node_colors,edge_color=edge_colors,style=edge_lines,node_size=100,connectionstyle="arc3,rad=0.1")
+    nx.draw(g,ax=ax,pos=layout,node_color=node_colors,edge_color=edge_colors,style=edge_lines,node_size=100,connectionstyle="arc3,rad=0.1",with_labels=True)
 
-#%%
 def draw_splits(G,split_list,node_map,node_color_rule,edge_color_rule,edge_line_rule):
-    fig,ax = plt.subplots(3,1,figsize=(10,30),squeeze=True)
+    fig,ax = plt.subplots(3,1,figsize=(10,30),squeeze=True, dpi=300)
     for i,split in enumerate(split_list):
         g = add_edges_from_pyg(G,split,node_map)
         if i == 0:
             layout = nx.kamada_kawai_layout(g)
         draw_graph(g,ax[i],node_color_rule,edge_color_rule,edge_line_rule,layout)
+
+def get_reverse_types(edge_types):
+    newlist = []
+    for edge in edge_types:
+        rev = tuple(reversed(edge))
+        if rev != edge:
+            if edge not in newlist:
+                newlist.append(rev)
+        else:
+            newlist.append(rev)
+
+    reversed_newlist = [tuple(reversed(edge)) for edge in newlist]
+
+    return newlist, reversed_newlist
+
 #%%
+#Load edge and node csv files
+data_folder = "../../data/processed/graph_data_nohubs/"
+
+node_data, node_map = load_node_csv(data_folder+"subgraph_nodes.csv","node_index","node_type")
+edge_data, edge_index = load_edge_csv(data_folder+"subgraph_edges.csv","x_index","y_index",node_map,"edge_type","x_type","y_type")
+
+#Initialize heterodata object
+data = create_heterodata(node_map,edge_index)
+#%%
+#Generate edge type lists to ensure even splitting between both edge directions
+edge_types, rev_edge_types = get_reverse_types(data.edge_types)
+
+#Split the dataset
+#Example of a good split for link prediction: disjoint split, consider reverse edges (prevents reversed edge from leaking into the split)
+good_transform = RandomLinkSplit(num_val=0.3, num_test=0.3, is_undirected=True, add_negative_train_samples=True, disjoint_train_ratio=0.2,edge_types=edge_types,rev_edge_types=rev_edge_types)
+
+#Exapmple of a bad split for link prediction: message passing and supervision edges can be shared (non disjoint), dont consider reverse edges
+bad_transform = RandomLinkSplit(num_val=0.3, num_test=0.3, is_undirected=True, add_negative_train_samples=True,edge_types=data.edge_types)
+
+g_train, g_val, g_test = good_transform(data)
+b_train, b_val, b_test = bad_transform(data)
+
 good_list = [g_train,g_val,g_test]
 bad_list = [b_train,b_val,b_test]
 #%%
-node_color_rule = {"disease":"black","bert_group":"black","gene_protein":"grey","complex":"grey","pathway":"grey"}
+#Initialize graph without edges
+G = nx.MultiDiGraph()
+G.add_nodes_from(node_data.index.values)
+nx.set_node_attributes(G,node_data["node_type"].to_dict(),"node_type")
+#%%
+#Plot splits with edges colored by subtype (message passing or supervision) and line style by supervision label
+node_color_rule = {"disease":"black","bert_group":"brown","gene_protein":"grey","complex":"grey","pathway":"grey"}
 edge_color_rule = {"message_passing_edges":"red","supervision_edges":"blue"}
 edge_line_rule = {1.0:"solid",0.0:"dashed"}
 
-draw_splits(G,bad_list,node_map,("node_type",node_color_rule),("subtype",edge_color_rule),("supervision_label",edge_line_rule))
+draw_splits(G,good_list,node_map,("node_type",node_color_rule),("subtype",edge_color_rule),("supervision_label",edge_line_rule))
 #%%
+#Plot splits with edges colored by type (gda,ppi,disease_disease, etc) and line style by supervision label
 node_color_rule = {"disease":"black","bert_group":"black","gene_protein":"grey","complex":"grey","pathway":"grey"}
-edge_color_rule = {"gda":"green","disease_disease":"red","ppi":"blue","pathway_protein":"blue","forms_complex":"blue"}
-edge_line_rule = {"message_passing_edges":"solid","supervision_edges":"dashed"}
+edge_color_rule = {"gda":"green","ppi":"blue","disease_disease":"red"}
+edge_line_rule = {1.0:"solid",0.0:"dashed"}
 
-draw_splits(G,good_list,node_map,("node_type",node_color_rule),("type",edge_color_rule),("subtype",edge_line_rule))
+draw_splits(G,good_list,node_map,("node_type",node_color_rule),("type",edge_color_rule),("supervision_label",edge_line_rule))
+#%%
